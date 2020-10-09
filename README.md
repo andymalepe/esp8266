2d14b9d7aad1480608f91a32b24232d14d2a5395
Kagiso Malepe <kagiso@overdrive.co.za>
2020-10-09 17:49:49 +0200

    WebServer service: in progress

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # esp8266
-D1 Mini 
+D1 Mini
 
 PlatformIO code
 
+Install ESP8266 library
 pio run
 pio run -t upload /dev/ttyUSB0
 
-Creates WiFi AC point.
-Create a Web Server for displaying SSID and Password for new AP
-Start logging data once connected
+### Creates WiFi AC point.
+### Create a Web Server for displaying SSID and Password for new AP
+### Start logging data once connected
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+#include <WiFiClient.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
 IPAddress localIP(192,168,4,2);
 IPAddress gateway(192,168,4,1);
 IPAddress subnet(255,255,255,0);
+
+ESP8266WebServer server(80);
+void index();
+void error();
 
 void setup(){
   pinMode(LED_BUILTIN, OUTPUT);
@@ -13,6 +19,8 @@ void setup(){
   while (WiFi.status() != WL_CONNECTED){
     delay(1000);
   }
+
+  server.on("/", HTTP_GET, index);
 }
 
 void loop(){
@@ -22,4 +30,10 @@ void loop(){
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
+}
+void index(){
+  server.send(200, "text/html", "<form action=\"/connect\" method=\"POST\"><input type=\"text\" name=\"ssid\" placeholder=\"Network SSID\"><input type=\"password\" name=\"password\" placeholder=\"Password\"><input type=\"submit\" value=\"Connect to AP\"></form>");
+}
+void error(){
+  server.send(404, "text/plain", "Error 404");
 }
